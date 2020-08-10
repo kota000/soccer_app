2029ea11af24c3c384cf35b632d9fa700a757c08
Kota Iio <117267w@gmail.com>
2020-08-10 18:36:02 +0900

    add: devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/kawasaki',      to: 'static_pages#kawasaki'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions' ,
+  }
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
 end
