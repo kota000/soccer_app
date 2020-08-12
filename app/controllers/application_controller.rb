d4ec96c96d3b5100e140f78520269a6d1ba3cd4d
Kota Iio <117267w@gmail.com>
2020-08-12 11:29:02 +0900

    add: usernameカラムの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters,if: :devise_controller?
+
+  # ログイン済ユーザーのみにアクセスを許可する
+  # before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :username, :password]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, length: { maximum: 10 }
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 end
