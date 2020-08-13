9f817761c36c56ba185a9621c06485fb80abc62f
Kota Iio <117267w@gmail.com>
2020-08-13 22:39:04 +0900

    add: carrierwave gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :username, :password]
+    added_attrs = [ :email, :username, :password, :image]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    root_path 
+    root_path
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 10 }
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+  mount_uploader :image, ImageUploader
 end
