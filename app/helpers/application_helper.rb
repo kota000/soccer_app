0e726d0adffb82c67f4f00822ec149248be247bd
Kota Iio <117267w@gmail.com>
2020-08-15 20:00:58 +0900

    add: 編集画面の画像表示追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title(page_title = {})
+    if page_title.blank?
+      APP_TITLE
+    else
+      "#{page_title} - #{APP_TITLE}"
+    end
+  end
 end
