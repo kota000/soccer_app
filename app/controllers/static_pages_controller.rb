2d092ec5fe1226c08057ee9e0823e9240ef23e00
Kota Iio <117267w@gmail.com>
2020-08-19 11:34:23 +0900

    add: teamページの作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,71 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
+  before_action :create_post, except: [:home]
   def home
   end
 
   def kawasaki
+  end
+
+  def goosaka
+  end
+
+  def seresso
+  end
+
+  def nagoya
+  end
+
+  def kasiwa
+  end
+
+  def fctokyo
+  end
+
+  def urawa
+  end
+
+  def hirosima
+  end
+
+  def sapporo
+  end
+
+  def koube
+  end
+
+  def yokohamafm
+  end
+
+  def kasima
+  end
+
+  def senndai
+  end
+
+  def simizu
+  end
+
+  def saganntosu
+  end
+
+  def ooita
+  end
+
+  def shounann
+  end
+
+  def yokohamafc
+  end
+
+  private
+
+  def create_post
     @posts = Post.all
     @post = current_user.posts.build if user_signed_in?
     if user_signed_in?
       @post = current_user.posts.build
-      # @feed_items = current_user.feed # .paginate(page: params[:page])
     end
   end
 end
