870be9310029d3d79c09b159d005da81b97a2a7c
Kota Iio <117267w@gmail.com>
2020-09-09 21:41:57 +0900

    fix: 管理者機能変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :username, presence: true, length: { maximum: 10 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
