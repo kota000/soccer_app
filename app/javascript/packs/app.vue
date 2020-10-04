6eabe0d5f5dc5595e4c1d13d13ed7606f7a34682
Kota Iio <117267w@gmail.com>
2020-10-04 12:16:50 +0900

    add: 表の表示追加

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,5 +1,27 @@
 <template>
   <div id="app">
-    Hello Vue!
+    <div class="container">
+      <table>
+        <thead>
+          <tr>
+            <th>順位</th>
+            <th>チーム名 </th>
+            <th>投票数 </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>１</td>
+            <td>川崎フロンターレ</td>
+            <td>sd</td>
+          </tr>
+          <tr>
+            <td>Day er</td>
+            <td>dsdqewfewfefef</td>
+            <td>sd</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
