dfd68b84ed84519378df7247269964bb40785432
Kota Iio <117267w@gmail.com>
2020-10-15 21:59:07 +0900

    add: question 修正＆テスト追加

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -18,11 +18,26 @@ RSpec.describe 'questions', type: :feature do
         expect(page).to have_content '優勝するチームを予想しよう！'
         expect(page).to have_content '〜2020年優勝すると思うチームを選択してください。〜'
       end
+
+      it '投票しランキングに反映され、投票ボタンが消えるか' do
+        find("option[value='浦和レッズ']").select_option
+        click_on '投票する'
+        expect(page).to have_content '投票しました。'
+        expect(page).to have_content '浦和レッズ'
+        expect(page).to have_content '総投票数:1件'
+        expect(page).not_to have_content '投票する'
+      end
+
+      it 'チームを選択しないと投票できない' do
+        click_on '投票する'
+        expect(page).to have_content '投票することができませんでした。'
+      end
     end
 
     context 'ログインしていない時' do
       it '表示確認' do
         expect(page).not_to have_content '〜2020年優勝すると思うチームを選択してください。〜'
+        expect(page).not_to have_content '投票する'
       end
     end
   end
